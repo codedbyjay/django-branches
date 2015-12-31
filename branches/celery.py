44b9b4a7c0ead22ae44e2380c97e01135e687c49
jaywhy13 <jeanmark.wright@gmail.com>
2015-12-31 02:02:21 -0500

    Use redis as our broker. Also added a get_new_task_id so we can use that in functions

--- branches/celery.py
+++ branches/celery.py
@@ -3,29 +3,27 @@ import os
 
 from django.conf import settings
 from celery import Celery
+from celery import uuid
 from celery.signals import celeryd_after_setup
 from celery.schedules import crontab
 from kombu import Exchange, Queue
-from autodiscovery_module import autodiscover
+
+def get_new_task_id():
+    return uuid()
 
 # Define some constants
-BROKER_USER = "branches"
-BROKER_PASSWORD = "branches"
-BROKER_HOST = "localhost"
-BROKER_PORT = 5672
-BROKER_URL = "amqp://%s:%s@%s:%s//" % \
-	(BROKER_USER, BROKER_PASSWORD, BROKER_HOST, BROKER_PORT)
+BROKER_URL = 'redis://localhost:6379/0'
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_branches.settings')
 
-app = Celery('excess')
+app = Celery('branches')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.update(
 	CELERY_TIMEZONE=settings.TIME_ZONE,
 	BROKER_URL=BROKER_URL,
-	CELERY_BACKEND="amqp",
 	CELERY_TASK_RESULT_EXPIRES=18000, # 5 hours
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 )
