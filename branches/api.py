cdc8e108001694c388185f735db85e645be80ce4
jaywhy13 <jeanmark.wright@gmail.com>
2015-12-31 01:59:17 -0500

    Added ChangeBranchRequestResource that gives information about the request.
    Whether it’s in progress, cancelled or complete.

--- branches/api.py
+++ branches/api.py
@@ -70,3 +70,30 @@ class ProjectResource(ModelResource):
         info = project.get_status(limit=limit)
         response = json.dumps(dict(info=info))
         return HttpResponse(response, content_type="application/json")
+
+
+
+class ChangeBranchRequestResource(ModelResource):
+    class Meta:
+        queryset = ChangeBranchRequest.objects.all()
+        resource_name = "change-branch-request"
+
+    def dehydrate(self, bundle):
+        obj = bundle.obj
+        bundle.data["complete"] = obj.complete
+        bundle.data["in_progress"] = obj.in_progress
+        bundle.data["cancelled"] = obj.cancelled
+        bundle.data["requested"] = obj.requested
+        change_branch_log = bundle.obj.change_branch_log
+        if change_branch_log:
+            bundle.data["log"] = change_branch_log.get_log(limit=15)
+        return bundle
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<pk>\d+)/log/$" % self._meta.resource_name, self.wrap_view('log'), name="log"),
+        ]
+
+    def log(self, request, pk, **kwargs):
+        change_branch_request = get_object_or_404(ChangeBranchRequest, pk=pk)
+        return HttpResponse(change_branch_request.change_branch_log.get_log(limit=1000))
