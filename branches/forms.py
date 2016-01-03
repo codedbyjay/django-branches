6540303e233c23373e2d072d766fb691dc6a4009
jaywhy13 <jeanmark.wright@gmail.com>
2016-01-03 15:11:49 -0500

    Added some imports and corrected a spelling issue

--- branches/forms.py
+++ branches/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Button
+from crispy_forms.layout import Layout, Submit, Button, HTML, Div
 from crispy_forms.bootstrap import FormActions
 
 from fabric.api import env, run, execute, cd
@@ -14,7 +14,7 @@ class NewServerForm(ModelForm):
     class Meta:
         model = Server
         fields = ["name", "address", "description", "port"]
-        exclude = ["intialized"]
+        exclude = ["initialized"]
 
     def __init__(self, *args, **kwargs):
         result = super(NewServerForm, self).__init__(*args, **kwargs)
