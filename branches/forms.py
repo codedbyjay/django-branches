f130046ae94d128643fccad71a247a1fe654d4fd
jaywhy13 <jeanmark.wright@gmail.com>
2016-01-03 15:14:52 -0500

    Did up an InitializeServerForm that allows the user to supply a username and password for their server.
    The clean method checks if we can get into the server using the
    credentials supplied.

--- branches/forms.py
+++ branches/forms.py
@@ -39,3 +39,45 @@ class NewServerForm(ModelForm):
         )
         return result
 
+class InitializeServerForm(forms.ModelForm):
+
+    class Meta:
+        model = Server
+        fields = ["address", "username", "port"]
+    address = forms.CharField(label="Server Address")
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    port = forms.IntegerField()
+
+    def __init__(self, server, *args, **kwargs):
+        self.server = server
+        result = super(InitializeServerForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    HTML("<span class='title'>Initialize server</span>"),
+                    HTML("<span class='instructions'>Now we need to initialize your server</span>"),
+                    css_class='form-header'),
+                'address',
+                'username',
+                'password',
+                'port',
+                Div(css_class='server-log'),
+                FormActions(
+                    Submit('login', 'Login'),
+                ),
+            css_class='branches-form'),
+        )
+        return result
+
+    def clean(self):
+        # check if the username and password work
+        cleaned_data = self.cleaned_data
+        address = cleaned_data.get("address")
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+        port = cleaned_data.get("port")
+        if not test_credentials(address, username, password, port=port):
+            raise forms.ValidationError("Ooops.. we didn't get into the server with those credentials")
+        return cleaned_data
