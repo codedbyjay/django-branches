b0688111a291250e9ed73389c05133e08ff0961a
jaywhy13 <jeanmark.wright@gmail.com>
2016-04-12 06:57:18 -0500

    Allowed us to specify in settings where the SSH key is kept

--- branches/system.py
+++ branches/system.py
@@ -3,20 +3,28 @@ import os
 from bash import bash
 
 def get_ssh_key():
-	key_location = os.path.expanduser("~/.ssh/id_rsa.pub")
-	if os.path.exists(key_location):
-		key = open(key_location).read()
-		return key
-	return None
+    key_location = settings.SSH_KEY_LOCATION
+    key_file = "%s/id_rsa.pub" % key_location
+    if os.path.exists(key_file):
+        key = open(key_file).read()
+        return key
+    return None
+
 
 def generate_ssh_key():
-	bash("ssh-keygen -t rsa -N '' -q -f ~/.ssh/id_rsa")
-	bash("ssh-keygen -f ~/.ssh/id_rsa -e -m pem > ~/.ssh/id_rsa.pub.pem")
+    key_location = settings.SSH_KEY_LOCATION
+    bash("ssh-keygen -t rsa -N '' -q -f %s/id_rsa" % key_location)
+    bash(
+        "ssh-keygen -f %s/id_rsa -e -m pem > %s/id_rsa.pub.pem"
+        % (key_location, key_location))
 
 key = get_ssh_key()
 if not key:
-	print("Generating a SSH key for you")
-	generate_ssh_key()
+    print("Generating a SSH key for you")
+    generate_ssh_key()
+
 
 def get_key_filename():
-	return os.path.expanduser("~/.ssh/id_rsa")
+    key_location = settings.SSH_KEY_LOCATION
+    key_file = "%s/id_rsa" % key_location
+    return key_file
