764a5773c84d2efd0145b4dae0ffc34674abafdf
jaywhy13 <jeanmark.wright@gmail.com>
2016-04-12 07:06:08 -0500

    Added change_branch_log as a property, cancel_change_branch, get the current branch, request_branch_change, change_branch, get last branch change request, added the CHangeBranchRequest properties and cancel method. get_log from ChangeBranchLog. also, clean the script, strip all the lines and replace with our \n

--- branches/models.py
+++ branches/models.py
@@ -9,7 +9,6 @@ from django.db.models.signals import pre_save
 from fabric.api import env, run, execute, cd, local, sudo
 from fabric.contrib.files import append
 from redis import Redis
-import django_rq
 from bash import bash
 
 from django_extensions.db.models import TimeStampedModel
@@ -18,6 +17,7 @@ from branches import *
 from branches.tasks import *
 from branches.networking import *
 from branches.git import Repo
+from branches.celery import get_new_task_id
 
 
 class Server(TimeStampedModel):
@@ -78,6 +78,22 @@ class Project(TimeStampedModel):
     def is_change_branch_requested(self):
         return self.change_branch_requests.\
             filter(status=ChangeBranchRequest.STATUS_REQUESTED).exists()
+
+    @property
+    def change_branch_log(self):
+        if self.is_changing_branch:
+            change_branch_log = self.change_branch_logs.filter(active=True)[0]
+            return change_branch_log.cancel()
+        return False
+
+
+    def cancel_change_branch(self):
+        if self.last_change_branch_request:
+            print("Cancelling last change branch request")
+            return self.last_change_branch_request.cancel()
+        print("No last change branch request, cannot cancel")
+        return False
+
     @property
     def repo(self):
         """ Return a repository object, only for use when connected to a server
@@ -86,6 +102,8 @@ class Project(TimeStampedModel):
 
     @property
     def current_branch(self):
+        """ Returns the branch the server is currently on
+        """
         with connect(self.server):
             def get_current_branch():
                 with cd(self.location):
@@ -112,39 +130,124 @@ class Project(TimeStampedModel):
                 with cd(self.location):
                     return self.repo.get_status(limit=limit)
             result = execute(get_status)
-            return result.get(self.server.address)
+            result = result.get(self.server.address)
+            try:
+                change_branch_log = self.change_branch_logs.latest("pk")
+                result["change_branch_log"] = change_branch_log.get_log(limit=limit)
+            except ChangeBranchLog.DoesNotExist:
+                pass
+            result["is_changing_branch"] = self.is_changing_branch
+            result["is_change_branch_requested"] = self.is_change_branch_requested
+            return result
         return None
-    
 
     def request_branch_change(self, branch):
-        result = django_rq.enqueue(change_server_branch, self.pk, branch)
+        if self.is_change_branch_requested:
+            raise Exception("A branch change was already requested.")
+        task_id = get_new_task_id() # generate a uuid
+        change_branch_request = ChangeBranchRequest.objects.create(
+            project=self,
+            branch=branch,
+            task_id=task_id)
+        task_args = [self.pk, branch, change_branch_request.pk]
+        result = change_server_branch.apply_async(args=task_args,
+            task_id=task_id)
+        return change_branch_request
 
-    def change_branch(self, branch):
-        number_of_changes = self.change_branch_logs.count()
-        script_filename = "%s/server_log_%s_%s.log" % \
-            (settings.MEDIA_ROOT, self.pk, number_of_changes)
+    def change_branch(self, branch, task_id, change_branch_request_pk):
+        """ This method is executed by the ``change_server_branch`` task in
+            the Celery process. 
+        """
+        active_change_branch_logs = self.change_branch_logs.filter(active=True)
+        for change_branch_log in active_change_branch_logs:
+            other_task_id = change_branch_log.task_id            
+            # check if this tsak is complete first
+            result = change_server_branch.AsyncResult(other_task_id)
+            if not result.ready():
+                print("Task %s is already running, cannot change branch" % other_task_id)
+                return False
+            # Set this as inactive then
+            change_branch_log.active = False
+            change_branch_log.save()
+        # Double check that none are active, in case another worker started
+        if self.change_branch_logs.filter(active=True).exists():
+            print("Someone beat us to it.. can't change branch")
+            return False
+
+        try:
+            change_branch_request = ChangeBranchRequest.objects.get(pk=change_branch_request_pk)
+            if not change_branch_request.requested:
+                """ At this point we expect the request to be requested, if it's 
+                    not it means that either:
+                    - Some other worker has actioned the request
+                    - It has been cancelled
+                """
+                print("Not actioning this request, it currently has status: %s" % \
+                        change_branch_request.status)
+                return False
+            print("Request status is: %s" % change_branch_request.status)
+        except ChangeBranchRequest.DoesNotExist:
+            print("If this request does not exist, don't continue")
+            return False
         current_branch = self.current_branch
+        # Create a chnge log and mark it as active
         change_branch_log = ChangeBranchLog.objects.create(project=self,
             previous_branch=current_branch,
             current_branch=branch,
-            log_filename=script_filename)
+            active=True)
+        change_branch_request.change_branch_log = change_branch_log
+        change_branch_request.status = ChangeBranchRequest.STATUS_IN_PROGRESS
+        change_branch_request.save()
+
+        # Mark all other as inactive
+        self.change_branch_logs.exclude(pk=change_branch_log.pk).\
+            update(active=False)
+
+        print("Asked to change branch from %s to %s" % (current_branch, branch))
+        # Make sure the media root directory exists
+        if not os.path.exists(settings.MEDIA_ROOT):
+            os.makedirs(settings.MEDIA_ROOT)
+        script_filename = "%s/server_log_%s_%s.log" % \
+            (settings.MEDIA_ROOT, self.pk, change_branch_log.pk)
+        user_script_filename = "script_%s.sh" % change_branch_log.pk
+        change_branch_log.log_filename = script_filename
+        change_branch_log.save()
         server = self.server
         with connect(server):
             with cd(self.location):
                 # Create the log file
-                local("touch %s" % script_filename)
-                log_file = open(script_filename, "a")
-                def change_branch_task():
-                    log_file.write("Changing branch from %s to %s\r\n" % \
-                            (current_branch, branch))
-                    log_file.write("Discarding local changes...\r\n")
-                    run("git checkout -- .", stdout=log_file)
-                    log_file.write("Changing branch to %s\r\n" % branch)
-                    run("git checkout %s" % branch, stdout=log_file)
-                    log_file.write("Running custom script\r\n")
-                execute(change_branch_task)
+                with open(script_filename, "w") as log_file:
+                    def change_branch_task():
+                        log_file.write("Changing branch from %s to %s\r\n" % \
+                                (current_branch, branch))
+                        log_file.write("Discarding local changes...\r\n")
+                        run("git checkout -- .", stdout=log_file)
+                        log_file.write("Changing branch to %s\r\n" % branch)
+                        run("git checkout %s" % branch, stdout=log_file)
+                        # check if the branch exists on origin
+                        run("git pull", stdout=log_file, warn_only=True)
+                        if self.change_branch_script:
+                            log_file.write("Running custom script\r\n")
+                            run("rm %s" % user_script_filename, warn_only=True)
+                            run("touch %s" % user_script_filename)
+                            append(user_script_filename, self.change_branch_script)
+                            run("chmod +x %s" % user_script_filename)
+                            sudo("./%s" % user_script_filename, stdout=log_file)
+                    try:
+                        execute(change_branch_task)
+                    except Exception as e:
+                        print("Exception occurred changing branches: %s" % e)
+                        log_file.write(e.message)
+                        change_branch_log.active = False
+                        change_branch_log.save()
+                        change_branch_request.status = ChangeBranchRequest.STATUS_COMPLETE
+                        change_branch_request.save()
+                        return False
+            print("Marking task as inactive now")
             change_branch_log.active = False
             change_branch_log.save()
+            change_branch_request.status = ChangeBranchRequest.STATUS_COMPLETE
+            change_branch_request.save()
         return True
 
     def get_commits(self, limit=10):
@@ -156,8 +259,70 @@ class Project(TimeStampedModel):
                 return result.get(self.server.address)
         return []
 
+    @property
+    def last_change_branch_request(self):
+        try:
+            return self.change_branch_requests.latest("created")
+        except ChangeBranchRequest.DoesNotExist:
+            return None
+
+    def update_branch_list(self):
+        with connect(self.server):
+            def fetch():
+                with cd(self.location):
+                    self.repo.fetch()
+                    return self.repo.available_branches
+            result = execute(fetch)
+            return result.get(self.server.address)
+        return None
+
 
 class ChangeBranchRequest(TimeStampedModel):
+
+    STATUS_REQUESTED = "requested"
+    STATUS_IN_PROGRESS = "in-progress"
+    STATUS_COMPLETE = "complete"
+    STATUS_CANCELLED = "cancelled"
+
+    project = models.ForeignKey("Project", related_name="change_branch_requests")
+    branch = models.CharField(max_length=255)
+    status = models.CharField("Status", max_length=50, default=STATUS_REQUESTED)
+    change_branch_log = models.OneToOneField("ChangeBranchLog", 
+        blank=True, null=True)
+    task_id = models.CharField(max_length=255, blank=True, null=True)
+
+    def cancel(self):
+        # Terminate the task 
+        result = change_server_branch.AsyncResult(self.task_id)
+        if not result.ready():
+            print("Terminating the task")
+            result.revoke(terminate=True)
+        if self.change_branch_log:
+            self.change_branch_log.active = False
+            self.change_branch_log.save()
+        print("Marking ourselves as cancelled")
+        self.status = ChangeBranchRequest.STATUS_CANCELLED
+        self.save()
+        return True
+
+
+    @property
+    def complete(self):
+        return self.status == ChangeBranchRequest.STATUS_COMPLETE
+
+    @property
+    def requested(self):
+        return self.status == ChangeBranchRequest.STATUS_REQUESTED
+
+    @property
+    def cancelled(self): 
+        return self.status == ChangeBranchRequest.STATUS_CANCELLED
+
+    @property
+    def in_progress(self):
+        return self.status == ChangeBranchRequest.STATUS_IN_PROGRESS
+
+
 class ChangeBranchLog(TimeStampedModel):
 
     project = models.ForeignKey("Project", related_name="change_branch_logs")
@@ -167,8 +332,31 @@ class ChangeBranchLog(TimeStampedModel):
     log_filename = models.FileField(blank=True, null=True)
     active = models.BooleanField(default=True)
 
-    def get_log(limit=50):
+    @property
+    def task_id(self):
+        return self.change_branch_request.task_id
+    
+
+    def get_log(self, limit=50):
         """ Gets the last limit lines of the log file
         """
-        result = bash("tail -n %s %s" % (limit, self.log_filename.url))
+        result = unicode(bash("tail -n %s %s" % (limit, self.log_filename.url)))
+        result = result.replace("\n", "<br>")
+        result = result.replace("\r", "")
         return result
+
+
+def clean_script(sender, instance, **kwargs):
+    """ Remove any weird spaces from the script
+    """
+    print("Cleaning script")
+    raw = kwargs.get("raw")
+    if raw:
+        return
+    script = instance.change_branch_script
+    if not script:
+        return
+    clean_line = lambda line: line.strip() + "\n"
+    script = "".join(map(clean_line, script.splitlines()))
+    instance.change_branch_script = script
+pre_save.connect(clean_script, sender=Project)
