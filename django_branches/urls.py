9b79ee0524929534a1e253e90118ef86908d8e0d
jaywhy13 <jeanmark.wright@gmail.com>
2016-04-12 07:02:56 -0500

    Added urls for login, dashboard and registration

--- django_branches/urls.py
+++ django_branches/urls.py
@@ -3,14 +3,28 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 
+from django_branches.views import LoginView, DashboardView, RegistrationView
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns(
+    '',
     # Examples:
     url(r'^$', 'django_branches.views.home', name='home'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
+    url(r'^accounts/register/$', RegistrationView.as_view(), name='register'),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     # Both urls needed for branches to work
     url(r'^branches/', include('branches.urls', namespace="branches")),
 
-) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
