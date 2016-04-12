8b9caacf68b80aac5fa32bb9d05ad32126ebbaf3
jaywhy13 <jeanmark.wright@gmail.com>
2016-04-12 06:58:37 -0500

    Added some logging to copy_key_to_server, save the port if changed.

--- branches/tasks.py
+++ branches/tasks.py
@@ -3,23 +3,36 @@ from fabric.contrib.files import append
 
 from .networking import connect, test_credentials
 from .system import get_ssh_key
+from branches.celery import app
 
+
+@app.task()
 def copy_key_to_server(server_pk, username, password, port):
+    """ Copies our key to the server
+    """
     from branches.models import Server
     server = Server.objects.get(pk=server_pk)
+    print("Connecting to %s with %s on port: %s" %
+          (server.address, username, port))
     if not test_credentials(server.address, username, password, port):
+        print("Credentials failed")
         return False
-
     with connect(server, username, password, port):
         print("Copying up the key now...")
         key = get_ssh_key()
-        append_key = lambda : append("~/.ssh/authorized_keys", key)
-        execute(append_key)
+        execute(lambda: append("~/.ssh/authorized_keys", key))
         server.initialized = True
+        # Update the port incase it changed
+        server.port = port
         server.save()
         return True
 
-def change_server_branch(project_pk, branch):
+
+@app.task(bind=True)
+def change_server_branch(self, project_pk, branch, change_branch_request):
     from branches.models import Project
     project = Project.objects.get(pk=project_pk)
-    return project.change_branch(branch)
+    return project.change_branch(
+        branch, self.request.id, change_branch_request)
+
+
