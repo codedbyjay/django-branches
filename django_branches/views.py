e809bcfa995ffeaba94b768366a060f5c0a9add8
jaywhy13 <jeanmark.wright@gmail.com>
2016-04-12 07:03:37 -0500

    Added views for the Dashboard, Registration and Login

--- django_branches/views.py
+++ django_branches/views.py
@@ -1,4 +1,46 @@
+from django.views.generic import TemplateView, FormView
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+from registration.backends.hmac.views import (
+    RegistrationView as CoreRegistrationView
+)
+
+from branches.forms import LoginForm, RegistrationForm
+
 
 def home(request):
-    return render_to_response("home.html")
+    return render_to_response("branches/home.html")
+
+
+class DashboardView(TemplateView):
+
+    template_name = "branches/dashboard.html"
+
+
+class RegistrationView(CoreRegistrationView):
+
+    form_class = RegistrationForm
+
+
+class LoginView(FormView):
+
+    form_class = LoginForm
+    template_name = "branches/login.html"
+
+    def form_valid(self, form):
+        cleaned_data = form.cleaned_data
+        email = cleaned_data.get("email")
+        password = cleaned_data.get("password")
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return redirect("dashboard")
+        else:
+            print("Invalid user")
+            form.add_error(None, "Invalid email or password")
+        context = self.get_context_data()
+        context["form"] = form
+        return self.render_to_response(context)
+
