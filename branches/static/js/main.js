b8fad607844b4a25cda3c947baf4b3921da95903
jaywhy13 <jeanmark.wright@gmail.com>
2016-04-12 06:44:54 -0500

    Set sripTrailingSlashes to false in the client rest API. Also added 'change branch'.

--- branches/static/js/main.js
+++ branches/static/js/main.js
@@ -4,15 +4,34 @@
 	});
 
 	var client = new $.RestClient("/branches/api/v1/");
-	client.add('project');
+	client.add('project', {
+		stripTrailingSlash : false,
+	});
 	client.project.addVerb('commits', 'GET');
 	client.project.addVerb('available-branches', 'GET');
 	client.project.addVerb('current-branch', 'GET');
 	client.project.addVerb('status', 'GET');
+	client.project.addVerb('changebranch', 'POST', {
+		stripTrailingSlash: false
+	});
+
+	// Utils
+	var reloadIn = function(seconds){
+		window.setTimeout(function(){
+			window.location.reload();
+		}, seconds * 1000)
+	};
+
+	var utils = {
+		"reloadIn" : reloadIn,
+	};
+
 
 	var branches = {
-		"client" : client
+		"client" : client,
+		"utils" : utils,
 	};
+
 
 	window.branches = branches;
 })();
